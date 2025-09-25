d74a425e892b69df01b0ef11a1e473b09e00a93c
Mancha Loka Srinivas <lokasrinivas.mancha@gmail.com>
2025-09-25 23:53:23 +0530

    Add files via upload

---
+++ gradingusingNested.py
@@ -0,0 +1,32 @@
+marks=int(input("enter your marks:"))
+
+if marks >= 35:
+    if marks >= 90:
+        print("you got a O, party leedha pushpa")
+    elif marks >= 75:
+        print(" you got a A, fine ra mama")
+    elif marks >= 60:
+        print("you got a B, okay")        
+
+    
+
+else:
+    print("you are Fail, sar sar ley ra ")    
+
+
+number=int(input("enter a number:"))
+if number>=0:
+    print("number is positive")
+    if number%2==0:
+        print("even number")
+          
+        if number>50:
+           print("greather than 50")
+        else:
+         print("less than 50")
+    else:
+     print("odd number")
+else:
+ print("number is ")
+    
+
